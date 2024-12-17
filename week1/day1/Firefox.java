7bc74cf217a7a6f8a4e7e4cff3db84dfba1c4706
munnifaz <75413774+munnifaz@users.noreply.github.com>
2024-12-17 14:53:46 +1100

    Add files via upload
    Week1_Assignments

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+		
+		System.out.println("The browser version is: " +browserVersion +"\n" + "The browsername is: "+browserName +"\n" + 
+		"Is the application status is visible ? :"+isVisible +"\n" + "The release year is: "+releaseYear + "\n" + "The browser logo is: " +browserLogo);
+		
+	}
+
+}
